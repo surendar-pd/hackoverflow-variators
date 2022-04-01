ceb2bc7c62e76d4463c212e5e4c0a86b04819ff2
chanakyha <vetrichanakyha2003@gmail.com>
2022-04-02 03:15:17 +0530

    Budget Page| Routing

--- pages/home/dashboard.js
+++ pages/home/dashboard.js
@@ -1,84 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import {useRouter} from 'next/router';
-import BottomNavi from '../../components/BottomNavi';
-import Header from '../../components/Header';
-import balanceSvg from '../../assets/balancw.svg';
-import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
-import Image from 'next/image'
-import {db, onSnapshot, doc, auth} from '../../utils/firebase';
-import { useAuthState} from "react-firebase-hooks/auth";
-import walletSvg from '../../assets/wallet.svg'
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import balanceSvg from "../../assets/balancw.svg";
+import Image from "next/image";
+import { db, onSnapshot, doc, auth } from "../../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import walletSvg from "../../assets/wallet.svg";
 
-const Dashboard = () => {
-
-  const [currentUser, setCurrentUser] = useState();
+const Dashboard = ({ currentUser }) => {
   const [user] = useAuthState(auth);
-  console.log(currentUser)
-
-
-  useEffect(() => {
-    if (user) {
-
-      onSnapshot(doc(db,"users",user?.uid),(snapshot) =>{
-        setCurrentUser(snapshot.data());
-      })
-    }
-
-  },[])
 
   const toIndianCurrency = (num) => {
-    const curr = num.toLocaleString('en-IN', {
-        style: 'currency',
-        currency: 'INR'
+    const curr = num.toLocaleString("en-IN", {
+      style: "currency",
+      currency: "INR",
     });
-  return curr;
-};
+    return curr;
+  };
 
-  const router = useRouter();
   const current = new Date();
-  current.setMonth(current.getMonth()-1);
-  const previousMonth = current.toLocaleString('default', { month: 'long' });
-
+  current.setMonth(current.getMonth() - 1);
+  const previousMonth = current.toLocaleString("default", { month: "long" });
 
   return (
-        <>
-          <div>
-            <h1 className="text-2xl">Hi <span className="font-bold">{user?.displayName}</span></h1>
+    <>
+      <div>
+        <h1 className="text-2xl">
+          Hi <span className="font-bold">{user?.displayName}</span>
+        </h1>
+      </div>
+      <div className="w-full h-3/4 flex justify-center items-center">
+        <div className="w-full py-2 h-fit flex gap-4 overflow-x-scroll snap-x">
+          <div className="w-64 h-80 p-4 bg-white shrink-0 rounded-lg shadow-md snap-center">
+            <h1>Current Balance</h1>
+            <h1 className="font-bold text-xl">
+              {toIndianCurrency(parseInt(currentUser?.balance))}
+            </h1>
+            <div className="flex justify-center items-center">
+              <Image width={200} height={200} src={balanceSvg} alt="balance" />
+            </div>
           </div>
-          <div className="w-full h-3/4 flex justify-center items-center">
-            <div className="w-full py-2 h-fit flex gap-4 overflow-x-scroll snap-x">
-              <div className="w-64 h-80 p-4 bg-white shrink-0 rounded-lg shadow-md snap-center">
-                <h1>Current Balance</h1>
-                <h1 className="font-bold text-xl">
-                  {toIndianCurrency(parseInt(currentUser?.balance))}
-                </h1>
-                <div className="flex justify-center items-center">
-                  <Image width={200} height={200} src={balanceSvg} alt="balance" />
-                </div>
+          <div className="w-64 h-80 p-4 bg-white shrink-0 rounded-lg shadow-md snap-center">
+            <h1>This Month Spends</h1>
+            <h1 className="font-bold text-xl">
+              {/* {toIndianCurrency(parseInt(currentUser?.balance))} */}
+              {toIndianCurrency(0)}
+            </h1>
+            <div className="flex justify-center items-center">
+              <Image width={175} height={175} src={walletSvg} alt="wallet" />
+            </div>
+            <div className="flex  mt-8 justify-between items-center">
+              <div>
+                <h1 className="font-semibold">{toIndianCurrency(250)}</h1>
+                <h1 className="text-xs">Dues for {previousMonth}</h1>
               </div>
-              <div className="w-64 h-80 p-4 bg-white shrink-0 rounded-lg shadow-md snap-center">
-                <h1>This Month Spends</h1>
-                <h1 className="font-bold text-xl">
-                  {/* {toIndianCurrency(parseInt(currentUser?.balance))} */}
-                  {toIndianCurrency(0)}
+              <div>
+                <h1 className="text-sm bg-[#008037] py-1 px-2 text-white rounded">
+                  Repay Now
                 </h1>
-                <div className="flex justify-center items-center">
-                  <Image width={175} height={175} src={walletSvg} alt="wallet" />
-                </div>
-                <div className="flex  mt-8 justify-between items-center">
-                  <div>
-                    <h1 className="font-semibold">{toIndianCurrency(250)}</h1>
-                    <h1 className="text-xs">Dues for {previousMonth}</h1>
-                  </div>
-                  <div>
-                    <h1 className="text-sm bg-[#008037] py-1 px-2 text-white rounded">Repay Now</h1>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
-        </>
-  )
-}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
